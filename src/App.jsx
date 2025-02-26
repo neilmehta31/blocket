53a3ed64640160554941b8a62ed0b8122507b5d8
Neil Mehta <neil.mehta310501@gmail.com>
2025-02-26 16:17:37 +0530

    WebBased wallet init

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { generateMnemonic } from "bip39";
+import { SolanaWallet } from './components/SolanaWallet';
+import { EthWallet } from './components/EthWallet';
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [mnemonic, setMnemonic] = useState("");
+
+  async function generateAndCopyMnemonic() {
+    const mn = await generateMnemonic();
+    setMnemonic(mn);
+    copyToClipboard(mn);
+  }
+  
+
+  function copyToClipboard(mn) {
+    navigator.clipboard.writeText(mn);
+  }
+
+  return (
+    <>
+      <button
+        onClick={generateAndCopyMnemonic}
+      >
+        Create Seed Phrase and copy to clipboard
+      </button>
+      <input type="text" value={mnemonic}></input>
+      <div className="wallets-container">
+        <SolanaWallet mnemonic={mnemonic==""? null :mnemonic}/>
+        <EthWallet mnemonic={mnemonic==""? null :mnemonic}/>
+      </div>
+    </>
+  );
+}     
+
+export default App
---
+++ src/components/EthWallet.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react"
+import { Wallet, HDNodeWallet } from "ethers";
+import { mnemonicToSeed } from "bip39";
+
+export const EthWallet = ({mnemonic}) => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [addresses, setAddresses] = useState([]);
+
+    if(mnemonic==null){
+        return(
+            <div >
+                <p>
+                    Generate a mnemonic first!
+                </p>
+            </div>
+        );
+    } else{
+      return (
+        <div>
+          <h2>Ethereum Wallet</h2>
+          <p>Mnemonic: {mnemonic}</p>
+          <button
+            onClick={async function () {
+              const seed = await mnemonicToSeed(mnemonic);
+              const derivationPath = `m/44'/60'/${currentIndex}'/0'`;
+              const hdNode = HDNodeWallet.fromSeed(seed);
+              const child = hdNode.derivePath(derivationPath);
+              const privateKey = child.privateKey;
+              const wallet = new Wallet(privateKey);
+              setCurrentIndex(currentIndex + 1);
+              setAddresses([...addresses, wallet.address]);
+            }}
+          >
+            Add ETH wallet
+          </button>
+
+          {addresses.map((p) => (
+            <div>Eth - {p}</div>
+          ))}
+        </div>
+      );
+    }
+}
